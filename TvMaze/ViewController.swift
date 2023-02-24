3ad8338ec97c8831d60454ec406bfb40afcd8ed3
Andre Vinicius Torres Conrado <vinicius.torresc@gmail.com>
2023-02-23 21:09:25 -0300

    Initial Commit

---
+++ TvMaze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TvMaze
+//
+//  Created by Vinicius Torres on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
